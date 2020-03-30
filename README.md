78b8d404e7095f64903e83906f616b553e0bdac7
mounwashere <cyliaberkane@gmail.com>
2020-03-30 16:45:13 +0200

    com

--- README.md
+++ README.md
@@ -17,13 +17,15 @@ python manage.py migrate
 
 #start the django server
 python manage.py runserver
+#the link to the website
+ http://127.0.0.1:8000/movies
+
 
 #Create exe file 
 pyinstaller --clean --name=movie manage.py
 
 
 #change directory to dist/movie
-cd dist/movie 
+cd dist/movies_register
 #execute file 
-
-
+movies_register.exe runserver localhost:8000
---
+++ movies_project/dist/movies_project/movies_project/movies_register/static/js/demo/chart-bar-demo.js
@@ -0,0 +1,112 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart");
+var myBarChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+
+    labels: labels = ["Action","Adventure","Animation", "Biography", "Comedy","Crime", "Drama", "Family", "Musical","Mystery", "Romance", "Science-Fiction","War","Western"],
+    datasets: [{
+      label: "Average Score",
+      backgroundColor: "#13ccdf",
+      hoverBackgroundColor: "#18d9d3",
+      borderColor: "#07b1df",
+      data:[5.689818181818182, 6.108496240601504, 6.7143456962911126, 6.253846153846154, 6.351351351351352, 6.161019417475728, 5.877777777777777, 6.757640232108317, 6.4799999999999995, 5.787096774193548, 5.792857142857143, 7.041456582633054, 6.348461538461539, 6.057142857142857, 6.2, 6.746931407942238, 7.4],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'days'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        },
+        maxBarThickness: 20,
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 10,
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: true
+    },
+    tooltips: {
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    },
+  }
+});
